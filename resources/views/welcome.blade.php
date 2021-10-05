c53b02b2e35bb62b85ea57e346789904529f0645
oliviervromans <olivier.vromans@hotmail.nl>
2021-10-05 15:30:08 +0200

    Css for Anime Cards

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,7 @@
 @extends('layouts.layout')
+@section('head')
+    <link rel="stylesheet" href="css/home.css">
+@endsection
 @section('header')
     <header class="py-5">
         <div class="container px-lg-5">
@@ -19,9 +22,31 @@
 @endsection
 @section('content')
 {{--    TODO DETAIL PAGE with get and ID element--}}
-    <ul>
-        @foreach($animes as $anime)
-            <li>{{$anime['title']}}</li>
-        @endforeach
-    </ul>
+{{--    <ul>--}}
+{{--        @foreach($animes as $anime)--}}
+{{--            <li>{{$anime['title']}}</li>--}}
+{{--            <img src="{{ asset("/storage/images/".$anime['image']) }}" alt="" height="300px">--}}
+{{--        @endforeach--}}
+{{--    </ul>--}}
+            <div id="container" class="container">
+                @foreach($animes as $anime)
+                <div class="cards" id="cards">
+                    <a href="#">
+                        <div class="card" id="card">
+                            <div class="image-box">
+                                <img src="{{ asset("/storage/images/".$anime['image']) }}" alt="" height="300px">
+                            </div>
+                            <div class="content-box">
+                                <h2>{{$anime['title']}}</h2>
+                                <div class="detail">
+                                    <span>220 ep</span>
+                                    <span>Fall 2020</span>
+                                </div>
+
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                @endforeach
+            </div>
 @endsection
